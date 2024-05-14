05577ff6bde87dd5c8982a4019a98c14fc4068a1
Juan Camilo Pacheco <jc.pacheco@uniandes.edu.co>
2024-05-14 08:24:21 -0500

    cambio cors

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import settings
 import openAIEmbeddings as oae
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/answer": {"origins": "https://manchita-front-a39e69c799a1.herokuapp.com"}})
+# cors = CORS(app, resources={r"/answer": {"origins": "https://manchita-front-a39e69c799a1.herokuapp.com"}})
+cors = CORS(app, origins='https://manchita-front-a39e69c799a1.herokuapp.com')
 
 openai.api_type = settings.APITYPE
 openai.api_base = settings.APIBASE
